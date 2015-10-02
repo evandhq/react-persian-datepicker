6b632c8a364c304fd9dadb5f0fa4404c613d05f0
mamal72 <m4m4lj@gmail.com>
2015-10-02 07:38:07 +0330

    add eslint & some lint error fixes

--- src/Day.js
+++ src/Day.js
@@ -2,36 +2,36 @@ import React, { PropTypes } from 'react';
 import { PersianNumber } from 'react-persian';
 
 export default class Day extends React.Component {
-    static propTypes = {
-        day: PropTypes.object.isRequired,
-        isCurrentMonth: PropTypes.bool,
-        isDisabled: PropTypes.bool,
-        selected: PropTypes.bool,
-        handleClick: React.PropTypes.func.isRequired
-    };
+  static propTypes = {
+    day: PropTypes.object.isRequired,
+    isCurrentMonth: PropTypes.bool,
+    isDisabled: PropTypes.bool,
+    selected: PropTypes.bool,
+    handleClick: React.PropTypes.func.isRequired
+  };
 
-    static defaultProps = {
-        isCurrentMonth: false
-    };
+  static defaultProps = {
+    isCurrentMonth: false
+  };
 
-    render() {
-        const { isDisabled, day, handleClick } = this.props;
-        let classes = ['day'];
+  render() {
+    const { isDisabled, day, handleClick } = this.props;
+    let classes = ['day'];
 
-        if (this.props.isCurrentMonth) {
-            classes.push('current-month');
-        }
-
-        if (this.props.selected) {
-            classes.push('selected');
-        }
-
-        classes = classes.join(' ');
-
-        return <div className="day-wrapper">
-            <button disabled={isDisabled} className={classes} onClick={handleClick}>
-                <PersianNumber>{day.format('jD')}</PersianNumber>
-            </button>
-        </div>;
+    if (this.props.isCurrentMonth) {
+      classes.push('current-month');
     }
+
+    if (this.props.selected) {
+      classes.push('selected');
+    }
+
+    classes = classes.join(' ');
+
+    return (<div className="day-wrapper">
+      <button disabled={isDisabled} className={classes} onClick={handleClick}>
+        <PersianNumber>{day.format('jD')}</PersianNumber>
+      </button>
+    </div>);
+  }
 }
--- src/Heading.js
+++ src/Heading.js
@@ -2,20 +2,21 @@ import React, { PropTypes } from 'react';
 import { PersianNumber } from 'react-persian';
 
 export default class Heading extends React.Component {
-    static propTypes = {
-        month: PropTypes.object.isRequired,
-        onNext: PropTypes.func.isRequired,
-        prevMonthElement: PropTypes.element.isRequired,
-        nextMonthElement: PropTypes.element.isRequired,
-    };
+  static propTypes = {
+    month: PropTypes.object.isRequired,
+    onNext: PropTypes.func.isRequired,
+    onPrev: PropTypes.func.isRequired,
+    prevMonthElement: PropTypes.element.isRequired,
+    nextMonthElement: PropTypes.element.isRequired
+  };
 
-    render() {
-        const { month, onPrev, onNext, prevMonthElement, nextMonthElement } = this.props;
+  render() {
+    const { month, onPrev, onNext, prevMonthElement, nextMonthElement } = this.props;
 
-        return <div className="heading">
-            <PersianNumber>{month.format('jMMMM jYYYY')}</PersianNumber>
-            <button className="prev-month" onClick={onPrev}>{prevMonthElement}</button>
-            <button className="next-month" onClick={onNext}>{nextMonthElement}</button>
-        </div>;
-    }
+    return (<div className="heading">
+      <PersianNumber>{month.format('jMMMM jYYYY')}</PersianNumber>
+      <button className="prev-month" onClick={onPrev}>{prevMonthElement}</button>
+      <button className="next-month" onClick={onNext}>{nextMonthElement}</button>
+    </div>);
+  }
 }
