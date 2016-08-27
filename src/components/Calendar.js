2c56a6f4ab43624a07a6280f425a14881a97de85
Mohamad Mohebifar <netfars@gmail.com>
2016-08-27 19:18:58 -0230

    fix: Fix issues with typing in the input, outside click detection and preserving focus on input. Also it makes it possible to pass a TimePicker component.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,21 +1,24 @@
 import React, { Component, PropTypes } from 'react';
-import Heading from './Heading';
+import DaysViewHeading from './DaysViewHeading';
 import DaysOfWeek from './DaysOfWeek';
 import MonthSelector from './MonthSelector';
 import Day from './Day';
 import { getDaysOfMonth } from '../utils/moment-helper';
 import moment from 'moment-jalali';
+import onClickOutside from 'react-onclickoutside';
 
 // Load Persian localisation
 moment.loadPersian();
 
-export default class Calendar extends Component {
+export class Calendar extends Component {
   static propTypes = {
     min: PropTypes.object,
     max: PropTypes.object,
+    styles: PropTypes.object,
     selectedDay: PropTypes.object,
     defaultMonth: PropTypes.object,
-    onSelect: PropTypes.func
+    onSelect: PropTypes.func,
+    onClickOutside: PropTypes.func
   };
 
   static childContextTypes = {
@@ -23,6 +26,10 @@ export default class Calendar extends Component {
     prevMonth: PropTypes.func.isRequired,
     setCalendarMode: PropTypes.func.isRequired,
     setMonth: PropTypes.func.isRequired
+  };
+
+  static defaultProps = {
+    styles: require('../styles/basic.css')
   };
 
   state = {
@@ -72,6 +79,8 @@ export default class Calendar extends Component {
 
   selectDay(selectedDay) {
     const { month } = this.state;
+
+    // Because there's no `m1.isSame(m2, 'jMonth')`
     if (selectedDay.format('jYYYYjMM') !== month.format('jYYYYjMM')) {
       this.setState({ month: selectedDay });
     }
@@ -87,17 +96,25 @@ export default class Calendar extends Component {
     }
   };
 
+  handleClickOutside(event) {
+    if (this.props.onClickOutside) {
+      this.props.onClickOutside(event);
+    }
+  }
+
   days = null;
   lastRenderedMonth = null;
 
   renderMonthSelector() {
     const { month } = this.state;
-    return (<MonthSelector selectedMonth={month}/>);
+    const { styles } = this.props;
+    return (<MonthSelector styles={styles} selectedMonth={month}/>);
   }
 
   renderDays() {
     const { month, selectedDay } = this.state;
-    const { min, max } = this.props;
+    const { children, min, max, styles, outsideClickIgnoreClass } = this.props;
+
     let days;
 
     if (this.lastRenderedMonth === month) {
@@ -110,9 +127,10 @@ export default class Calendar extends Component {
 
     return (
       <div>
-        <Heading month={month}/>
-        <DaysOfWeek/>
-        <div className="calendar-container">
+        {children}
+        <DaysViewHeading styles={styles} month={month}/>
+        <DaysOfWeek styles={styles}/>
+        <div className={styles.dayPickerContainer}>
           {
             days.map(day => {
               const isCurrentMonth = day.format('jMM') === month.format('jMM');
@@ -121,12 +139,13 @@ export default class Calendar extends Component {
 
               return (
                 <Day
-                  key={day.format('YYYYMMDD')}
+                  key={day.format('YYYYMMDD') }
                   onClick={this.handleClickOnDay}
                   day={day}
                   disabled={disabled}
                   selected={selected}
                   isCurrentMonth={isCurrentMonth}
+                  styles={styles}
                 />
               );
             })
@@ -137,14 +156,24 @@ export default class Calendar extends Component {
   }
 
   render() {
-    const { min, max, ...rest } = this.props;
+    const {
+      selectedDay,
+      min,
+      max,
+      onClickOutside,
+      outsideClickIgnoreClass,
+      styles,
+      className,
+      ...rest
+    } = this.props;
     const { mode } = this.state;
 
     return (
-      <div className={'calendar'} {...rest}>
+      <div className={styles.calendarContainer + ' ' + className} {...rest}>
         { mode === 'monthSelector' ? this.renderMonthSelector() : this.renderDays() }
       </div>
     );
   }
 }
 
+export default onClickOutside(Calendar);
