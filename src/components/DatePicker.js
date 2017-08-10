1aff54003b8f60724520e16274f7a4b735f479e1
Ali Qanavatian <a_good_per@yahoo.com>
2017-08-10 16:35:39 +0430

    fix propTypes deprecation issue (#36)

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import moment from 'moment-jalali';
 import TetherComponent from 'react-tether';
 import Calendar from './Calendar';
